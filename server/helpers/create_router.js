a165cbe831a1578a37ac0a558797007485007e0c
marekboro <marek.borowicz83@gmail.com>
2020-09-02 12:15:32 +0100

    started on router

---
+++ server/helpers/create_router.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { Router } = require('express');
+const ObjectID = require('mongodb').ObjectID;
+
+const createRouter = function(collection) {
+    const router = express.Router();
+
+    router.get('/', (req,res) => {
+        collection.find().toArray()
+        .then(docs => res.json(docs))
+        .catch((err) => {
+            res.status(500);
+            res.json({ status: 500, error: err })
+          })
+      
+    })
+}
