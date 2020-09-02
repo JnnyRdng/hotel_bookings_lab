9127d636a09322e524fdcf24d4925aa5400dc57d
marekboro <marek.borowicz83@gmail.com>
2020-09-02 14:19:59 +0100

    added layout, styling and components to App

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,23 +1,48 @@
 <template>
-<div>
+<div id="main">
+    <h3>Hello from the main side</h3>
     <section id="formSection">
-           <bookingForm>
-
-
-
-           
+        <h4>Hello from the form section</h4>
+           <booking-form>     
     </section>
     <section id="displayBookings">
-
+        <h4>Hello from the booking list section</h4>
+        <booking-list> 
     </section>
 </div>
-
-
 </template>
 
 <script>
-export default {};
+import BookingForm from '@components/BookingForm.vue';
+import BookingList from '@components/BookingList.vue';
+
+export default {
+    name: 'app',
+    components: {
+        'booking-form': BookingForm,
+        'booking-list': BookingList
+    }
+};
 </script>
 
 <style>
+#main {
+    padding: 0%;
+    margin: 0%;
+    display: flex;
+    flex-direction: column;
+    border: solid black 2px;
+
+}
+#formSection{
+    padding: 0%;
+    margin: 0%;
+    border: solid rgb(0, 71, 30) 2px;
+}
+#formSection{
+    padding: 0%;
+    margin: 0%;
+    border: solid rgb(0, 71, 4) 2px;
+}
+
 </style>
