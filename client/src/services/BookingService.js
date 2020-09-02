f59ef64dfff533e3ddd2c518cb3a7d61777d9c46
Jonny Reading <jonnyreading@gmail.com>
2020-09-02 14:48:27 +0100

    booking service functions

---
+++ client/src/services/BookingService.js
@@ -0,0 +1,22 @@
+const baseURL = "http://localhost:3000/api/hotel_bookings";
+
+export default {
+    getBookings() {
+        return fetch(baseURL).then(
+            res => res.json()
+        );
+    },
+    addBooking(payload) {
+        return fetch(baseURL, {
+            method: "POST",
+            headers: { "ContentType": "application/json" },
+            body: JSON.stringify(payload)
+        })
+            .then(res => res.json());
+    },
+    deleteBooking(id) {
+        return fetch(baseURL + id, {
+            method: "DELETE"
+        });
+    }
+}
