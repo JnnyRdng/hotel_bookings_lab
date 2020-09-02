24c8d34861ac24fecd186ec62f828af89ca87b28
marekboro <marek.borowicz83@gmail.com>
2020-09-02 14:05:39 +0100

    started work on app

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,4 +1,19 @@
-<template></template>
+<template>
+<div>
+    <section id="formSection">
+           <bookingForm>
+
+
+
+           
+    </section>
+    <section id="displayBookings">
+
+    </section>
+</div>
+
+
+</template>
 
 <script>
 export default {};
